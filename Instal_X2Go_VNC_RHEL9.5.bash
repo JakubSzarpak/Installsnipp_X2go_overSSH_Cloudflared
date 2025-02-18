474d2a4b60bed66b402652f58b4481629b41f34a
Jakub Szarpak <97069547+JakubSzarpak@users.noreply.github.com>
2025-02-18 13:35:05 +0100

    Add files via upload

---
+++ Instal_X2Go_VNC_RHEL9.5.bash
@@ -0,0 +1,34 @@
+1. 
+
+sudo dnf config-manager --set-enabled crb
+
+sudo dnf install epel-release
+
+sudo dnf install x2goserver x2goserver-xsession
+
+sudo dnf groupinstall "Server with GUI" --->>>> VERIFY
+
+
+##############################################################
+
+3. Configure X2Go to Use GNOME:
+
+When setting up a new session in the X2Go client:
+
+Session Type: Select "Custom Desktop"
+Command: Enter gnome-session
+
+
+##############################################################
+
+sudo systemctl set-default graphical.target
+sudo systemctl enable gdm
+sudo systemctl start gdm
+
+##############################################################
+
+sudo firewall-cmd --add-service=ssh --permanent
+sudo firewall-cmd --reload
+
+
+##############MODIFY PORT IN sshd_config!!!!!!!!!!! ############
